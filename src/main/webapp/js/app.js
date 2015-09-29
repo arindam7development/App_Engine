0a868e5d26ec4f010e2c7af5738418df1062b24e
Arindam Mishra <arindam5191@gmail.com>
2015-09-29 11:59:00 +0530

    Update app.js

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -117,6 +117,15 @@ app.factory('oauth2Provider', function ($modal) {
      * Revokes all of the scopes that the user granted.
      * 
      * 
+     * 
+     * function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
+     *}
+     * 
+     * 
      */
     oauth2Provider.signOut = function () {
         gapi.auth.signOut();
